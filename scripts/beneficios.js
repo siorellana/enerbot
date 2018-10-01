889b745b769670411842b198113eff618352e1d6
Sebastian Orellana <sebastianorellana@hotmail.es>
2018-10-01 12:37:11 -0300

    Se agregan beneficios de trabajar en Enegon

---
+++ scripts/beneficios.js
@@ -0,0 +1,29 @@
+// Description:
+//   Beneficios: :enegon: es la mejor empresa y nos entrega un beneficio a la vez.
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   un beneficio - entrega un beneficio a la vez.
+
+var beneficios = [
+  'Tómate la tarde libre, proletario.',
+  '¡4 semanas de vacaciones pagadas!',
+  'Escoje el computador y la silla que quieras.',
+  'Snacks, café, frutas y bebidas libre todos los días todo el día.',
+  'Aguinaldo en septiembre y diciembre.',
+  'Bono anual por metas cumplidas.',
+  'Seguro de salud y dental para ti y tu familia.',
+  'Bono anual sólo por ser un :huemul: dev y/o golden :monea:',
+  'Permisos para celebrar y compartir la :beerjs:'
+];
+
+module.exports = function(robot) {
+  robot.respond(/un beneficio/gi, function(res) {
+    res.send( res.random(beneficios) );
+  });
+};
