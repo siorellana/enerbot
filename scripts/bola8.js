2f5219d7d678adb5b39cb4a3f0692e5b11703756
Sebastian Orellana <sebastianorellana@hotmail.es>
2018-10-01 12:43:13 -0300

    Se agrega script bola8

---
+++ scripts/bola8.js
@@ -0,0 +1,42 @@
+// Description:
+//   Enerbot te da un consejo según las frases de la Bola 8 (en español).
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   enerbot pregunta|consejo  - te da un consejo según las frases de la Bola 8
+
+var consejos = [
+  'En mi opinión, sí',
+  'Es cierto',
+  'Es decididamente así',
+  'Probablemente',
+  'Buen pronóstico',
+  'Todo apunta a que sí',
+  'Sin duda',
+  'Sí',
+  'Sí - definitivamente',
+  'Debes confiar en ello',
+  'Respuesta vaga, vuelve a intentarlo',
+  'Pregunta en otro momento',
+  'Será mejor que no te lo diga ahora',
+  'No puedo predecirlo ahora',
+  'Concéntrate y vuelve a preguntar',
+  'No cuentes con ello',
+  'Mi respuesta es no',
+  'Mis fuentes me dicen que no',
+  'Las perspectivas no son buenas',
+  'Muy dudoso'
+];
+
+module.exports = function(robot) {
+  robot.respond(/pregunta|consejo/gi, function(res) {
+
+    res.send( ':energon: ~ ' + res.random(consejos) );
+
+  });
+};
