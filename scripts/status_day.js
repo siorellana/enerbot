47844f329cf0910d2db1a923b73fa7c42473ad83
Sebastian Orellana <sebastianorellana@hotmail.es>
2018-10-01 15:16:18 -0300

    Se agrega que tal el dia

---
+++ scripts/status_day.js
@@ -0,0 +1,24 @@
+// Description:
+//   Enerbot te da un consejo según las frases de la Bola 8 (en español).
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   que tal el dia  - Te dice com va el día
+
+var consejos = [
+    ':fire::evilparrot:fire:',
+    
+  ];
+  
+  module.exports = function(robot) {
+    robot.respond(/que tal el dia/gi, function(res) {
+  
+      res.send( ':energon: ~ ' + res.random(consejos) );
+  
+    });
+  };
